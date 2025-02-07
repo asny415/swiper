30909f65a90cae271fcc95561a66a5c2ec10e3cc
wesley <9610848@qq.com>
2025-02-07 22:47:58 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fun.wqiang.swiper"
         minSdk = 25
         35.also { targetSdk = it }
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
